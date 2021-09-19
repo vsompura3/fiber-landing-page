852f40a41a8cba873eebe7a00f81801daf908b0b
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2021-09-19 23:15:30 +0530

    Added HTML Structure

--- script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.navbar__hamburger');
-const navigation = document.querySelector('.navbar__nav-items');
-
-function handleClick() {
-  navigation.classList.toggle('open');
-}
-
-hamburger.addEventListener('click', handleClick);
